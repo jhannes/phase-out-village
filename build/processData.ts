268ecd4ddb0e801de91db210bc2033ad6d15497f
Johannes Brodwall <johannes@brodwall.com>
2025-06-15 20:51:12 +0200

    bootstrap react application with typescript, husky and prettier

--- build/processData.ts
+++ build/processData.ts
@@ -10,23 +10,31 @@ const result: Record<
   >
 > = {};
 
-for (const [field, year, gwh, productionOil, productionGas, _, emission] of rows) {
+for (const [
+  field,
+  year,
+  gwh,
+  productionOil,
+  productionGas,
+  _,
+  emission,
+] of rows) {
   const dataPoint = { productionOil, productionGas, emission };
   for (const k of Object.keys(dataPoint)) {
-    if (!dataPoint[k]) delete dataPoint[k];
+    if (!(dataPoint as any)[k]) delete (dataPoint as any)[k];
   }
-  if (Object.keys(dataPoint)) (result[field] ||= {})[year] = dataPoint;
+  if (Object.keys(dataPoint)) ((result[field] as any) ||= {})[year] = dataPoint;
 }
 
 const compactJson = JSON.stringify(result, null, 2).replace(
-    /{([^{}]+)}/g,
-    (match, contents) =>
-        `{ ${contents
-            .split('\n')
-            .map(line => line.trim())
-            .filter(Boolean)
-            .join(' ')} }`
-);;
+  /{([^{}]+)}/g,
+  (match, contents) =>
+    `{ ${(contents as string)
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean)
+      .join(" ")} }`,
+);
 
 console.log(
   "export const data: Record<string, Record<string, { productionOil?: number; productionGas?: number; emission?: number }>> = " +
