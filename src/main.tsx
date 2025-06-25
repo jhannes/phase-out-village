e52f752f17550e018e7fe07c3bd7856be1c1edc0
Johannes Brodwall <johannes@brodwall.com>
2025-06-25 20:34:02 +0200

    start showing some data

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { data } from "./generated/data";
 
-createRoot(document.getElementById("app")!).render(
-  <h1>Chill, React! Chill!</h1>,
-);
+function Application() {
+  return (
+    <div>
+      <h1>Chill, baby! Chill!</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>År</th>
+            {Object.keys(data).map((oilField) => (
+              <th>{oilField}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data["Aasta Hansteen"]).map((y) => (
+            <tr>
+              <td>{y}</td>
+              {data["Aasta Hansteen"][y].productionOil}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <pre>{JSON.stringify(Object.keys(data["Draugen"]), null, 2)}</pre>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("app")!).render(<Application />);
