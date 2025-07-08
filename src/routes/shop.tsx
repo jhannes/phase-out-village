533073df030ec98382f61b366418c6112d3d4206
aleksejunas <photo@seventyseven.no>
2025-07-09 00:03:06 +0200

    Solved commit conflicts

--- src/routes/shop.tsx
+++ src/routes/shop.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 import React from "react";
-=======
-import React from "react
->>>>>>> Stashed changes
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/shop")({
