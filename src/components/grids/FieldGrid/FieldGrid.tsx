70fe6faa41553e56a8be2dc24ede55eb55da64ab
aleksejunas <photo@seventyseven.no>
2025-07-14 17:51:05 +0200

    fixed typeerrors in FieldGrid.tsx
    Errors are gone

--- src/components/grids/FieldGrid/FieldGrid.tsx
+++ src/components/grids/FieldGrid/FieldGrid.tsx
@@ -6,26 +6,33 @@ interface Field {
   phasedOut: boolean;
 }
 
-const FieldGrid = ({ fields, selectedFields, onSelectField }: { fields: Field[], selectedFields: string[], onSelectField: (id: string) => void }) => {
+const FieldGrid = ({
+  fields,
+  selectedFields,
+  onSelectField,
+}: {
+  fields: Field[];
+  selectedFields: string[];
+  onSelectField: (id: string) => void;
+}) => {
   return (
     <div className="field-grid">
       {fields.map((field) => (
-          <div
-            key={field.id}
-            className={`field ${field.phasedOut ? "phased-out" : ""}`}
-          >
-            <input
-              type="checkbox"
-              checked={selectedFields.includes(field.id)}
-              disabled={field.phasedOut}
-              onChange={() => onSelectField(field.id)}
-            />
-            <span>{field.name}</span>
-          </div>
-        ))}
-      </div>
-    );
-  }
+        <div
+          key={field.id}
+          className={`field ${field.phasedOut ? "phased-out" : ""}`}
+        >
+          <input
+            type="checkbox"
+            checked={selectedFields.includes(field.id)}
+            disabled={field.phasedOut}
+            onChange={() => onSelectField(field.id)}
+          />
+          <span>{field.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default FieldGrid;
-
