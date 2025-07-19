1eb12a71ba21451b1345fe88d49ce3f419ae40e9
Johannes Brodwall <johannes@brodwall.com>
2025-07-19 11:57:33 +0200

    feature: application structure

--- src/routes/application.tsx
+++ src/routes/application.tsx
@@ -1,13 +1,31 @@
 import { Link, Outlet } from "@tanstack/react-router";
-import React from "react";
+import React, { useState } from "react";
 
 export function Application() {
+  const [year, setYear] = useState(2025);
+
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> |<Link to="/map">Map</Link>
+        <Link to="/">Home</Link>
+        <Link to="/map">Map</Link>
+        <Link to="/emissions">Utslipp</Link>
+        <Link to="/production">Produksjon</Link>
       </nav>
-      <Outlet />
+      <header>
+        <div>
+          År: {year}
+          <div>
+            <button onClick={() => setYear((y) => y + 1)}>Neste</button>
+          </div>
+        </div>
+        <div>Oljefelter avviklet: 0</div>
+        <div>Utslipp til 2040: 200 (0% redusjon)</div>
+        <div>Produksjon til 2040: 200 (0% redusjon)</div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
